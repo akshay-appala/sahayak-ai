456be157d94c0e3b6e197a0fa03c3fa207ee6559
akshay-appala <akshaysunny2833@gmail.com>
2026-07-16 19:36:14 +0530

    Build responsive frontend layout and eligibility form

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
 app.use("/", routes);
 
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log("SahayakAI Loaded");
